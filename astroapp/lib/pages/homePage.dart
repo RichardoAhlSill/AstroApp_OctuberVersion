3937b1907f40cb9bd6bb980bc3278063eaf5e35a
Icami747 <103399414+Icami747@users.noreply.github.com>
2022-09-06 07:43:35 -0300

    Update homePage.dart

--- astroapp/lib/pages/homePage.dart
+++ astroapp/lib/pages/homePage.dart
@@ -125,7 +125,7 @@ class _HomePageState extends State<HomePage> {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => NoticiasPage(),
+                              builder: (context) => HomePage(),
                             ),
                           );
                         }),
